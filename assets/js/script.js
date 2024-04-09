882629bd22b9c5089d8087e7c70808ed3448ae1c
stankajofre <157607276+stankajofre@users.noreply.github.com>
2024-04-09 15:40:51 -0400

    requerimiento 1 implementando iife

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,5 @@
 // 1. Implementar el Patrón Módulo mediante IIFE
-const reproductor = (() => {
+const miFuncionIIfe = (() => {
     // Función privada para mostrar el video en el documento HTML
     const mostrarVideo = (url, id) => {
         const iframe = document.getElementById(id);
@@ -17,54 +17,3 @@ const reproductor = (() => {
     };
 })();
 
-// 2. Clase padre Multimedia
-class Multimedia {
-    constructor(url) {
-        let _url = url; // Atributo protegido implementado con closures
-
-        // Método para realizar un cambio en la URL del video
-        this.setInicio = () => {
-            return "Este método es para realizar un cambio en la URL del video";
-        };
-
-        // Getter para acceder al atributo url
-        this.getUrl = () => _url;
-
-        // Setter para modificar el atributo url
-        this.setUrl = (newUrl) => {
-            _url = newUrl;
-        };
-    }
-}
-
-// 3. Clase hija Reproductor
-class Reproductor extends Multimedia {
-    constructor(url, id) {
-        super(url);
-        this.id = id;
-    }
-
-    // Método para mostrar el video en la etiqueta iframe correspondiente
-    playMultimedia() {
-        Reproductor.playMultimedia(this.getUrl(), this.id);
-    }
-
-    // Método para modificar el tiempo de inicio en la URL de la etiqueta iframe
-    setInicio(tiempo) {
-        const url = this.getUrl() + `?start=${tiempo}`;
-        this.setUrl(url);
-    }
-}
-
-// 4. Instanciar la clase hija Reproductor y mostrar los videos
-const musica = new Reproductor("https://www.youtube.com/embed/VIDEO_MUSICA", "iframeMusica");
-const pelicula = new Reproductor("https://www.youtube.com/embed/VIDEO_PELICULA", "iframePelicula");
-const serie = new Reproductor("https://www.youtube.com/embed/VIDEO_SERIE", "iframeSerie");
-
-// Invocar al método playMultimedia para cada instancia creada
-musica.playMultimedia();
-pelicula.playMultimedia();
-serie.playMultimedia();
-
-// Modificar el tiempo de inicio en alguna de las instancias creadas
-serie.setInicio(30); // Se inicia el video de la serie en el segundo 30
